5caedceda8131efd26773efbe8c6b4231b269dea
unknown <igwe martin>
2024-07-19 09:36:32 +0100

    my_aws...

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -10,7 +10,7 @@ export async function POST(req) {
             region: 'us-east-1',
             credentials: {
                 accessKeyId: process.env.AWS_ACCESS_KEY,
-                secretAccessKey: process.env.AWS_SECRET_KEY,
+                secretAccessKey: process.env.   MY_AWS_SECRET_KEY,
             },
         });
 
