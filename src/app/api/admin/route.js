535c783f35902e6202e73da9e390457186968400
unknown <igwe martin>
2024-07-23 12:01:30 +0100

    fixes

--- src/app/api/admin/route.js
+++ src/app/api/admin/route.js
@@ -1,3 +1,5 @@
+import {UserInfo} from "@/models/UserInfo";
+import { authOptions } from "../auth/[...nextauth]/route";
 export async function isAdmin() {
     const session = await getServerSession(authOptions);
     const userEmail = session?.user?.email;
